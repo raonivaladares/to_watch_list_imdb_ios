682ee12e4da75f71ecb5e1f9e55b955b600de1da
rov <valadares.thel@gmail.com>
2016-04-03 16:00:02 -0300

    Adds segue with movie data between search and details

--- to_watch_list_imdb_ios/MovieDetailsViewController.swift
+++ to_watch_list_imdb_ios/MovieDetailsViewController.swift
@@ -1,5 +1,19 @@
 import UIKit
 
 class MovieDetailsViewController: UIViewController {
-
+  // MARK: atributes
+  var movie: Movie?
+  
+  // MARK: outlets
+  @IBOutlet weak var imageMovieCover: UIImageView!
+  @IBOutlet weak var labelMovieTitle: UILabel!
+  @IBOutlet weak var labelMovieCategory: UILabel!
+  @IBOutlet weak var labelMovieDescription: UILabel!
+  
+  override func viewDidLoad() {
+    //imageMovieCover
+    labelMovieTitle.text = movie?.name
+   // labelMovieCategory.text = movie?.name
+   // /labelMovieDescription.text = movie?.name
+  }
 }
--- to_watch_list_imdb_ios/SearchMovieViewController.swift
+++ to_watch_list_imdb_ios/SearchMovieViewController.swift
@@ -2,48 +2,49 @@
 import UIKit
 
 class SearchMovieViewController: UIViewController {
-    var alert = HelperAlert.sharedInstance
-    var movie : Movie?
-    var searchService: SearchService = SearchService.instance
-
-    @IBOutlet weak var textFieldMovieToSearch: UITextField!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+  // MARK: atributes
+  var alert = HelperAlert.sharedInstance
+  var movie : Movie?
+  var searchService: SearchService = SearchService.instance
+  
+  override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    if(segue.identifier == "MoviewDetailsSegue") {
+      
+      let viewController = (segue.destinationViewController as! MovieDetailsViewController)
+      viewController.movie = movie
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
-    @IBAction func touchButtonSearch(sender: UIButton) {
-      if let movieName = textFieldMovieToSearch.text where !movieName.isEmpty {
-        searchMovie(movieName)
-      } else {
-        alert.message(self, message: "Empty field: type the movie name")
+  }
+  
+  func searchMovie(movieName: String) {
+    searchService.searchMovie(movieName) { result in
+      var msg: String
+      switch (result) {
+      case .Success(let output):
+        if output.response == "True" {
+          msg = output.name
+          self.movie = output
+          self.performSegueWithIdentifier("MoviewDetailsSegue", sender: nil)
+        } else {
+          msg = output.error
+          self.alert.message(self, message: msg)
+        }
+      case .Failure(let error):
+        msg = error.localizedDescription
+        self.alert.message(self, message: msg)
       }
     }
-    
-  func searchMovie(movieName: String) {
-        searchService.searchMovie(movieName) { result in
-            var msg: String
-            switch (result) {
-            case .Success(let output):
-                if output.response == "True" {
-                  msg = output.name
-                  self.performSegueWithIdentifier("MoviewDetailsSegue", sender: nil)
-                } else {
-                  msg = output.error
-              }
-            case .Failure(let error):
-                msg = error.localizedDescription
-            }
-            self.alert.message(self, message: msg)
-        }
+  }
+
+  // MARK: outlets
+  @IBOutlet weak var textFieldMovieToSearch: UITextField!
+  
+  // MARK: actions
+  @IBAction func touchButtonSearch(sender: UIButton) {
+    if let movieName = textFieldMovieToSearch.text where !movieName.isEmpty {
+      searchMovie(movieName)
+    } else {
+      alert.message(self, message: "Empty field: type the movie name")
     }
-    
+  }
 }
 
