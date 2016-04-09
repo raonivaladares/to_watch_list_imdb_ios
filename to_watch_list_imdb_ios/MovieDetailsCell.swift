b2a7f826c7475f85ef746b710968babfa526b57c
rov <valadares.thel@gmail.com>
2016-04-09 14:31:22 -0300

    Adding new layout(incomplete)

--- to_watch_list_imdb_ios/MovieDetailsCell.swift
+++
@@ -1,7 +0,0 @@
-import UIKit
-
-class MovieDetailsCell: UITableViewCell {
-
-  @IBOutlet weak var labelDetail: UILabel!
-  @IBOutlet weak var labelDetailContent: UILabel!
-}
--- to_watch_list_imdb_ios/MovieDetailsViewController.swift
+++ to_watch_list_imdb_ios/MovieDetailsViewController.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-class MovieDetailsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class MovieDetailsViewController: UIViewController {
   // MARK: atributes
   var movie: Movie?
   
@@ -9,9 +9,8 @@ class MovieDetailsViewController: UIViewController, UITableViewDelegate, UITable
   // MARK: outlets
   @IBOutlet weak var imageMovieCover: UIImageView!
   @IBOutlet weak var labelMovieTitle: UILabel!
-  @IBOutlet weak var labelMovieGenreAndYear: UILabel!
+  @IBOutlet weak var labelMovieGenre: UILabel!
   @IBOutlet weak var labelMoviePlot: UILabel!
-  @IBOutlet weak var tableViewDetails: UITableView!
   
   override func viewDidLoad() {
     movieDetails = (movie?.moviewDetailsMap())!
@@ -21,27 +20,7 @@ class MovieDetailsViewController: UIViewController, UITableViewDelegate, UITable
     }
     
     labelMovieTitle.text = movie?.title
-    labelMovieGenreAndYear.text = movie?.genre
+    labelMovieGenre.text = movie?.genre
     labelMoviePlot.text = movie?.plot
-  }
-  
-  // number of rows in table view
-  func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return movieDetails.count
-  }
-  
-  // create a cell for each table view row
-  func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-    let cellIdentifier = "MovieDetailsCell"
-    let cell = tableViewDetails.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath) as! MovieDetailsCell
-    cell.labelDetail.text = Array(movieDetails.keys)[indexPath.row]
-    cell.labelDetailContent.text = Array(movieDetails.values)[indexPath.row]
-
-    return cell
-  }
-  
-  // method to run when table view cell is tapped
-  func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-    print("You tapped cell number \(indexPath.row).")
   }
 }
